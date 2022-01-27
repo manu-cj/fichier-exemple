94531c7e60805e8bf8cdc2f90f1491288b9a8f66
manucarenjot <carenjotje@gmail.com>
2022-01-27 10:04:58 +0100

    finis

--- index.php
+++ index.php
@@ -10,14 +10,49 @@
 
 
 <?php
-foreach ($_FILES['fichierUtilisateur'] as $key => $value) {
-    echo "$key => $value <br>";
-}
+/**
+ * Réception sans sécurité
+ */
 
 if (isset($_FILES["fichierUtilisateur"])) {
-    $tmp_name = $_FILES['fichierUtilisateur']["tmp_name"];
+    if ($_FILES['fichierUtilisateur']['error']===0) {
+        $tmp_name = $_FILES['fichierUtilisateur']["tmp_name"];
 
-    $name = $_FILES['fichierUtilisateur']["name"];
+        $name = $_FILES['fichierUtilisateur']["name"];
 
-    move_uploaded_file($tmp_name, $name);
+        move_uploaded_file($tmp_name, $name);
+        foreach ($_FILES['fichierUtilisateur'] as $key => $value) {
+            echo "$key => $value <br>";
+        }
+    }
+    else {
+        echo 'error';
+    }
 }
+
+/**
+ * Réception avec sécurité
+ */
+
+if (isset($_FILES["fichierUtilisateur"])) {
+    $allowedMimeTypes = ['text/plain', 'image/jpeg', 'image/png'];
+    if (in_array($_FILES['fichierUtilisateur']['type'], $allowedMimeTypes)) {
+        if ($_FILES['fichierUtilisateur']['error']===0) {
+            $tmp_name = $_FILES['fichierUtilisateur']["tmp_name"];
+
+            $name = $_FILES['fichierUtilisateur']["name"];
+
+            move_uploaded_file($tmp_name, $name);
+            foreach ($_FILES['fichierUtilisateur'] as $key => $value) {
+                echo "$key => $value <br>";
+            }
+        }
+        else {
+            echo 'error';
+        }
+    }
+   else {
+       echo 'le type du fichier n\'est pas autorisé';
+   }
+}
+
